d1b3ccc98565742a27dc8c92281bbe18c0862f9e
Bruno César Vilela <bruno23cv@gmail.com>
2023-03-13 20:38:12 -0300

    Add files via upload

---
+++ aula02/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Aula02 {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Bruno César Vilela");
+            if(args.GetLength(0)>0) {
+                Console.WriteLine(args.GetValue(0));
+            }
+        }
+    }
+}
